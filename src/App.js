0f3eabe40ba8109d7d9993440c3155da59c54645
bohongu <jaehong2782@naver.com>
2022-06-30 20:14:04 +0900

    fragment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Movies from "./routes/Movies";
 import Movie from "./routes/Movie";
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Movies />} />
         <Route path="/movies/:id" element={<Movie />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
-
-export default App;
